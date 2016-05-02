182657091ffec35b23620c3a86dc6fd425a335fc
Robert Wittman <rob@ihsdigital.com>
2016-05-02 17:22:33 -0400

    Fixed typo

--- lib/Auth.php
+++ lib/Auth.php
@@ -7,15 +7,15 @@ class Auth
     public static function authorizationUrl()
     {
         $params = array(
-            'redirect_uri'  => \Shopify\Shopify::redirectUrl(),
-            'client_id'     => \Shopify\Shopify::getApiKey(),
-            'scope'         => \Shopify\Shopify::getPermissions()
+            'redirect_uri'  => \Shopify\Shopify::$redirect_uri,
+            'client_id'     => \Shopify\Shopify::$api_key,
+            'scope'         => \Shopify\Shopify::$permissions
         );
         return sprintf("https://%s/admin/%s?%s", self::$store, self::$auth_uri, http_build_query($params));
     }
 
     public static function accessToken()
     {
-        
+
     }
 }

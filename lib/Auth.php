f43884262fbd78c6e62d35ea0dc911c1e97d90a4
Robert Wittman <rob@ihsdigital.com>
2016-05-02 17:21:20 -0400

    Fixed typo

--- lib/Auth.php
+++ lib/Auth.php
@@ -4,46 +4,18 @@ namespace Shopify;
 
 class Auth
 {
-    public static $api_key;
-    public static $api_secret;
-    public static $redirect_uri;
-    public static $permissions;
-    public static $store;
-    public static $nonce = NULL;
-    public static $auth_uri = 'oauth/authorize';
-    public static $token_uri = 'oauth/access_token';
-
-    public static function init(array $opts)
-    {
-        if(!array_key_exists('api_key', $opts) ||
-           !array_key_exists('api_secret', $opts) ||
-           !array_key_exists('redirect_uri', $opts) ||
-           !array_key_exists('permissions', $opts) ||
-           !array_key_exists('store', $opts))
-        {
-            throw new \Exception("Initialization options missing for \Shopify\Auth::init()");
-        }
-        self::$api_key = $opts['api_key'];
-        self::$api_secret = $opts['api_secret'];
-        self::$redirect_uri = $opts['redirect_uri'];
-        self::$permissions = $opts['permissions'];
-        self::$store = $opts['store'];
-        if(array_key_exists('nonce', $opts)) self::$nonce = $opts['nonce'];
-    }
-
     public static function authorizationUrl()
     {
         $params = array(
-            'redirect_uri'  => self::$redirect_uri,
-            'client_id'     => self::$api_key,
-            'scope'         => self::$permissions
+            'redirect_uri'  => \Shopify\Shopify::redirectUrl(),
+            'client_id'     => \Shopify\Shopify::getApiKey(),
+            'scope'         => \Shopify\Shopify::getPermissions()
         );
-        if(!is_null(self::$nonce)) $params['nonce'] = self::$nonce;
         return sprintf("https://%s/admin/%s?%s", self::$store, self::$auth_uri, http_build_query($params));
     }
 
     public static function accessToken()
     {
-
+        
     }
 }
--- lib/Shopify.php
+++ lib/Shopify.php
@@ -1,136 +1,28 @@
 <?php
 
-/**
- * This is the core handler for all Shopify API requests.
- *
- * The only metadata that this class needs to start executing requests is
- * a store to request on, and an access token to authenticate with. The API
- * key and secret are only used to get these access tokens
- *
- * 
- */
 namespace Shopify;
 
 class Shopify
 {
-    /**
-     * API Key used for requests
-     * @var string
-     */
     public static $api_key;
-
-    /**
-     * API Secret
-     *
-     * Only needed during OAuth requests
-     * @var string
-     */
     public static $api_secret;
+    public static $redirect_uri;
+    public static $permissions;
+    public static $store;
+    public static $nonce = NULL;
+    public static $auth_uri = '/admin/oauth/authorize';
+    public static $token_uri = '/admin/oauth/access_token';
 
-    /**
-     * Domain for requests
-     * @var strings
-     */
-    public static $store_domain;
-
-    /**
-     * Access Token for a given store
-     * @var string
-     */
-    public static $access_token;
-
-    /**
-     * API Version to user for requests
-     * @var float
-     */
-    public static $api_version = NULL;
-
-    /**
-     * API Execution options
-     * @var array
-     */
-    public static $api_options = array();
-
-    /**
-     * Retrieve our API Key
-     * @return string
-     */
-    public static function getApiKey()
+    public static function init(array $opts)
     {
-        return self::$api_key;
+        self::setOpts($opts);
     }
 
-    /**
-     * Retrieve the API Key used to instantiate our API Handler
-     * @return string
-     */
-    public static function getApiSecret()
+    public static function setOpts(array $opts)
     {
-        return self::$api_secret;
-    }
-
-    /**
-     * Set our API Ket
-     * @param void
-     */
-    public static function setApiCreds($key, $secret)
-    {
-        self::$api_key = $key;
-        self::$api_secret = $secret;
-    }
-
-    /**
-     * Set the API Version to user for requests
-     * @param float $version
-     */
-    public static function setApiVersion($version)
-    {
-        self::$api_version = $version;
-    }
-
-    /**
-     * Retrieve the API version for requests
-     * @return [type] [description]
-     */
-    public static function getApiVersion()
-    {
-        return self::$api_version;
-    }
-    /**
-     * Set the store to make API requests on behalf of
-     * @param string $store
-     * @param string $token  Store Access Token
-     */
-    public static function setStore($store, $token = NULL)
-    {
-        self::$store_domain = $store;
-        self::$access_token = $access_token;
-    }
-
-    /**
-     * Retrieve the store we've intialized the API for
-     * @return string
-     */
-    public static function getStore()
-    {
-        return self::$store_domain;
-    }
-
-    /**
-     * Retrieve the token we've initialized the API for
-     * @return string
-     */
-    public static function getAccessToken()
-    {
-        return self::$access_token;
-    }
-
-    public static function baseUrl()
-    {
-        if(is_null(self::$store_domain))
+        foreach($opts as $key => $value)
         {
-            throw new Exception("Calling API without instantiating you store. Use \\Shopify\\Shopify::setStore(store, access_token) to bootstrap");
+            self::$$key = $value;
         }
-        return sprintf("https://%s/admin/", self::$store_domain);
     }
 }

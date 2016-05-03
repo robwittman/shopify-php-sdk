d577321a13bf5177a28421f3154f149b5d50ab79
Robert Wittman <rob@ihsdigital.com>
2016-05-03 16:29:09 -0400

    Initial Commit

--- lib/Shop.php
+++ lib/Shop.php
@@ -6,11 +6,6 @@ class Shop extends AbstractObject
 {
     protected static $classUrl = 'shop';
 
-    public function __construct()
-    {
-
-    }
-
     /**
      * This is overridden since the shop is a singleton
      * object in Shopify's domain.

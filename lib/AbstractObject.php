c0645ae022ce08b29de8c655e4faea65b2ef6473
Robert Wittman <rob@ihsdigital.com>
2016-05-03 16:59:05 -0400

    Initial commit

--- lib/AbstractObject.php
+++ lib/AbstractObject.php
@@ -7,11 +7,5 @@ use Shopify\Util;
 
 abstract class AbstractObject extends AbstractResource
 {
-    public function __construct($data = array())
-    {
-        foreach($data->shop as $key => $value)
-        {
-            $this->{$key} = $value;
-        }
-    }    
+    
 }
--- lib/Product.php
+++ lib/Product.php
@@ -8,6 +8,14 @@ class Product extends AbstractObject
 {
     protected static $classUrl = 'products';
 
+    public function __construct($data = array())
+    {
+        foreach($data->product as $key => $value)
+        {
+            $this->{$key} = $value;
+        }
+    }
+
     public function all()
     {
         return self::call(self::$classUrl, 'GET');

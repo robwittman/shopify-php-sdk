93973fcf328eea2805cbeac4c457c3fce63013a2
Robert Wittman <rob@ihsdigital.com>
2016-05-09 14:17:28 -0400

    Initial Commit

---
+++ build.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+chdir(dirname((__FILE__));
+
+if($autoload) {
+    passthru(
+        './vendor/bin/phpunit -c phpunit.xml', $returnStatus
+    );
+    if($reutnrStatus !== 0)
+    {
+        exit(1);
+    }
+}
+exit(0);

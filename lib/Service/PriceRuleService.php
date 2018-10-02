7f2c176bae5b64d37b90389d978a749df803a5ce
Robert Wittman <robkwittman@gmail.com>
2018-10-01 20:57:02 -0700

    Add price rule service (#24)
    * Add price rule service functions
    
    * Update PriceRuleFields
    
    * Fix typo in DiscountCodeService

--- lib/Service/PriceRuleService.php
+++ lib/Service/PriceRuleService.php
@@ -6,5 +6,51 @@ use Shopify\Object\PriceRule;
 
 class PriceRuleService extends AbstractService
 {
-    
+    public function all(array $params = array())
+    {
+        $endpoint = '/admin/price_rules.json';
+        $response = $this->request($endpoint, 'GET', $params);
+        return $this->createCollection(PriceRule::class, $response['price_rules']);
+    }
+
+    public function get($priceRuleId, array $fields = array())
+    {
+        $params = array();
+        if (!empty($fields)) {
+            $params['fields'] = $fields;
+        }
+        $endpoint = '/admin/price_rules/'.$priceRuleId.'.json';
+        $response = $this->request($endpoint, 'GET', $params);
+        return $this->createObject(PriceRule::class, $response['price_rule']);
+    }
+
+    public function create(PriceRule &$priceRule)
+    {
+        $data = $priceRule->exportData();
+        $endpoint = '/admin/price_rules.json';
+        $response = $this->request(
+            $endpoint,
+            'POST',
+            array('price_rule' => $data)
+        );
+        $priceRule->setData($response['price_rule']);
+    }
+
+    public function update(PriceRule &$priceRule)
+    {
+        $data = $priceRule->exportData();
+        $endpoint = '/admin/price_rules/'.$priceRule->id.'.json';
+        $response = $this->request(
+            $endpoint, 'PUT', array(
+                'price_rule' => $data
+            )
+        );
+        $priceRule->setData($response['price_rule']);
+    }
+
+    public function delete(PriceRule $priceRule)
+    {
+        $endpoint = '/admin/price_rules/'.$priceRule->id.'.json';
+        $this->request($endpoint, 'DELETE');
+    }
 }

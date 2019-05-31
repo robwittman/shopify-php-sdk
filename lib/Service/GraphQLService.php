c3ef1a9e229f628388eeb12df0516b75d4355774
Andrew McCauley <andrew.mccauley@shopify.com>
2019-05-31 11:20:54 -0400

    add-basic-graphql-functionality

---
+++ lib/Service/GraphQLService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Shopify\Service;
+
+class GraphQLService extends AbstractService
+{
+
+  public function graph(string $query, array $variables = [])
+  {
+    // Build the request
+    $request = ['query' => $query];
+    if (count($variables) > 0) {
+        $request['variables'] = $variables;
+    }
+
+    // Create the request, pass the access token and optional parameters
+    $endpoint = '/admin/api/graphql.json';
+    $response = $this->request(
+        $endpoint,
+        'POST',
+        $request
+    );
+
+    return (object) [
+        'response'   => $response
+    ];
+  }
+}

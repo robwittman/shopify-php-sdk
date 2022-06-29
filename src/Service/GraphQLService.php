1e2384085fb28c7093f03e50857d4ced9abb521a
ha.ptt <phamhabnkx@gmail.com>
2022-06-29 16:52:30 +0700

    Update: throttle graphql

--- src/Service/GraphQLService.php
+++ src/Service/GraphQLService.php
@@ -6,8 +6,9 @@ class GraphQLService extends AbstractService
 {
   const MAX_TRIES = 10;
 
-  public function graph(string $query, array $variables = [], $tries = 0)
+  public function graph(string $query, array $variables = [], $max_tries = 0)
   {
+    $max_tries = $max_tries ?? self::MAX_TRIES;
     // Build the request
     $request = ['query' => $query];
     if (count($variables) > 0) {
@@ -16,19 +17,18 @@ class GraphQLService extends AbstractService
 
     // Create the request, pass the access token and optional parameters
     $endpoint = 'graphql.json';
-    $response = $this->request(
-        $endpoint,
-        'POST',
-        $request
-    );
+    $tries = 0;
 
-    if ($error = data_get($response, 'errors')) {
+    do {
+        $response = $this->request(
+            $endpoint,
+            'POST',
+            $request
+        );
+
+        $error = data_get($response, 'errors');
         $code = data_get($error, '0.extensions.code');
-        if ($code === 'THROTTLED' && $tries < self::MAX_TRIES) {
-            sleep(1);
-            return $this->graph($query, $variables, $tries + 1);
-        }
-    }
+    } while ($code === 'THROTTLED' && $tries++ < $max_tries);
 
     return (object) [
         'response'   => $response

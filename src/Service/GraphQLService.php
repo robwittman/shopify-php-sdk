635d00d8cf4620c2c6d5e9423e39e5f29f795b2c
ha.ptt <phamhabnkx@gmail.com>
2022-06-29 16:15:56 +0700

    Update: throttle graphql api

--- src/Service/GraphQLService.php
+++ src/Service/GraphQLService.php
@@ -4,8 +4,9 @@ namespace Shopify\Service;
 
 class GraphQLService extends AbstractService
 {
+  const MAX_TRIES = 10;
 
-  public function graph(string $query, array $variables = [])
+  public function graph(string $query, array $variables = [], $tries = 0)
   {
     // Build the request
     $request = ['query' => $query];
@@ -20,6 +21,14 @@ class GraphQLService extends AbstractService
         'POST',
         $request
     );
+
+    if ($error = data_get($response, 'errors')) {
+        $code = data_get($error, '0.extensions.code');
+        if ($code === 'THROTTLED' && $tries < self::MAX_TRIES) {
+            sleep(1);
+            return $this->graph($query, $variables, $tries + 1);
+        }
+    }
 
     return (object) [
         'response'   => $response
